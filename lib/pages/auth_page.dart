973b541b5b76a80285cbb08cce395e350ac973ca
Élenn Milhomem <elenndypaulla@hotmail.com>
2022-05-04 12:01:41 -0300

    Implementa tela de autenticação #02
    Mudanças de cores do gradiente e rotação do container com o titulo da aplicação

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -12,16 +15,47 @@ class AuthPage extends StatelessWidget {
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Color.fromRGBO(215, 117, 255, 0.5),
-                  Color.fromRGBO(255, 188, 117, 0.9),
+                  Color.fromRGBO(241, 144, 9, 1),
+                  Color.fromRGBO(242, 218, 167, 1),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
           SizedBox(
             width: double.infinity,
             child: Column(
-              children: const [],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xFF252422),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 10,
+                          color: Colors.black45,
+                          offset: Offset(0, 4),
+                        )
+                      ]),
+                  child: Text(
+                    'STANDUP GYM',
+                    style: GoogleFonts.titilliumWeb(
+                      textStyle: const TextStyle(
+                        fontSize: 30,
+                        color: Color(0xFFFFFCF2),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
